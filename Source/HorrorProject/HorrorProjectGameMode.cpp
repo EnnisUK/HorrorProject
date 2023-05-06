cf1c05b1de769d6b17839d8c61bd71bca28ea969
Ennis <A0095091@live.tees.ac.uk>
2023-05-07 00:12:45 +0100

    Deleted template work

--- Source/HorrorProject/HorrorProjectGameMode.cpp
+++ Source/HorrorProject/HorrorProjectGameMode.cpp
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "HorrorProjectGameMode.h"
-#include "HorrorProjectCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AHorrorProjectGameMode::AHorrorProjectGameMode()
